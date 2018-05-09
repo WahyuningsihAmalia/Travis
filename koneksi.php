3406148e9a71e5ee79333883d3befa7b23498f52
WahyuningsihAmalia <wahyuningsihamalia14@gmail.com>
2018-05-09 17:00:18 +0700

    travis

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class Koneksi {
     private $server = "localhost";
-    private $username = "root";//root
-    private $password = "";//""
-    private $db = "phb_kelas_c";
+    private $username = "id4905150_stok_barang";//root
+    private $password = "wahyuningsih";//""
+    private $db = "id4905150_stok_barang";
 
     function getKoneksi(){
         return new mysqli($this->server, $this->username,$this->password, $this->db);
